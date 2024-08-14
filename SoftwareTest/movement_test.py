abbba055ff0ee1ee7adf871f8bb42f9913b15d01
Winnie Liu <hl2393@cornell.edu>
2024-08-13 21:23:00 -0400

    Update movement_test.py

--- SoftwareTest/movement_test.py
+++ SoftwareTest/movement_test.py
@@ -1,9 +1,22 @@
 import math
 
+# Arm 1: First segment of arm, connected to base
+arm1_len = 1.5
+arm1_pos = [0.0, arm1_len]
+arm1_ang = 90.0
+
+# Arm 2: Second segment of arm, in the middle
+arm2_len = 1.0
+arm2_pos = [0.0, arm1_len + arm2_len]
+arm2_ang = 90.0
+
+# Arm 3: Third segment of arm, connected to hand
+arm3_len = 1.0
+arm3_pos = [0.0, arm1_len + arm2_len + arm3_len]
+arm3_ang = 90.0
+
 car_pos = [[0.0, 0.0]] # List of all cartesional positions
 pol_pos = [[0.0, 0.0]] # List of all polar positions
-
-car_list = [[1.0, 2.0], [4.0, 6.0], [-1.0, 3.0]] # List of all additional positions in cartesional
 
 # Convert cartensional cords to polar
 def car_to_pol(x, y):
@@ -11,24 +24,31 @@ def car_to_pol(x, y):
     t = math.atan(y/x)
     return [r, t]
 
-# Print cartesional cords
-def print_car(pos):
-    s = ""
-    for cord in pos:
-        s += ("(" + str(cord[0]) + ", " + str(cord[1]) + "), ")
-    print(s)
+# Convert degrees to radians
+def deg_to_rad(x):
+    return (x * math.pi) / 180
 
-# Print polar cords in cartesional form
-def print_pol(pos):
-    s = ""
-    for cord in pos:
-        s += ("(" + str(cord[0] * math.cos(cord[1])) + ", " + str(cord[0] * math.sin(cord[1])) + "), ")
-    print(s)
+def calc_len(p1, p2):
+    return math.sqrt(((p2[0] - p1[1])**2) + ((p2[0] - p1[1])**2))
 
-# Add positions to both lists
-for pos in car_list:
-    car_pos.append(pos)
-    pol_pos.append(car_to_pol(pos[0], pos[1]))
+# Calculate angle of line segment from origin to (x, y)
+# TODO: Make calculations with double var to avoid dependance on origin and x axis
+def pos_to_ang(x, y):
+    return math.tan(y/x)
 
-print_car(car_pos)
-print_pol(pol_pos)
+# Calculate position of line segment of length r at angle a
+# TODO: Make calculations with double var to avoid dependance on origin and x axis
+def ang_to_pos(r, a):
+    return [r * math.acos(a), r * math.asin(a)]
+
+def calc_arm_pos(x, y):
+    new_arm_pos = [[arm1_pos[0], arm1_pos[1]], [arm2_pos[0], arm2_pos[1]], [arm3_pos[0], arm3_pos[1]]]
+    new_arm_ang = [arm1_ang, arm2_ang, arm3_ang]
+    target_ang = pos_to_ang(x, y)
+
+    # Calc arm1 position
+    while calc_len([x, y], new_arm_pos[0]) > (arm2_len + arm3_len):
+        new_arm_ang[0] = (target_ang + new_arm_ang[0]) / 2
+        new_arm_pos[0] = ang_to_pos(arm3_len, new_arm_ang[0])
+
+    # TODO: Add logic to move arm2 and arm3

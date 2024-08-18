177cbc94ad75c0536300e2bed2f736418c338af7
Winnie Liu <hl2393@cornell.edu>
2024-08-18 15:25:31 -0400

    Seperated angles and position calculation functions

--- SoftwareTest/movement_test.py
+++ SoftwareTest/movement_test.py
@@ -1,4 +1,3 @@
-import copy
 import math
 
 # Arm angle and data
@@ -41,9 +40,6 @@ def cyl_to_car(r, t, z):
 def calc_len(p1, p2):
     return math.sqrt(((p2[0] - p1[0])**2) + ((p2[1] - p1[1])**2))
 
-def calc_len_3d(p1, p2):
-    return math.sqrt(((p2[0] - p1[0])**2) + ((p2[1] - p1[1])**2) + ((p2[2] - p1[2])**2))
-
 # Calculate angle using Cosine Rule
 def calc_ang(a, b, c):
     num = (-1 * a**2) + b**2 + c**2
@@ -58,46 +54,67 @@ def car_pos():
     return pos
 
 # Calculate the arm angle and positions to reach cartesian cords (x,y,z)
-def calc_arm_pos(x, y, z):
-    new_ang = copy.deepcopy(arm_ang)  # Copy all arm angles
-    new_pos = copy.deepcopy(arm_pos)  # Copy all arm positions
-
+def calc_arm_ang(x, y, z):
     r, t = car_to_pol(x, y)  # Turn cartersional to polar cords
 
+    # Calculate target information
     target_ang = math.pi / 2  # Default value if r == 0
     if r != 0.0:
         target_ang = math.atan(z/r)  # Angle above table of the target
     target_len = calc_len([0, 0], [r, z])  # Length from base to target
 
-    # Update base rotater's angle, position remains constant
-    new_ang[0] = t
+    # Set base rotater's angle
+    arm0_ang = t
 
-    # Update first hinge's angle and position
-    new_ang[1] = (3 * math.pi) / 4
-    new_pos[1] = [arm_len[1] * math.cos(new_ang[1]), arm_len[1] * math.sin(new_ang[1])]
-    hinge1_target_len = calc_len([r, z], new_pos[1])
+    # Calculate first hinge's angle
+    arm1_ang = (3 * math.pi) / 4  # Default angle
+    arm1_pos = [arm_len[1] * math.cos(arm1_ang), arm_len[1] * math.sin(arm1_ang)]
+    hinge1_target_len = calc_len([r, z], arm1_pos)
     # Ensure arm can reach target
     while hinge1_target_len > (arm_len[2] + arm_len[3]):
-        new_ang[1] -= (new_ang[1] - target_ang) * 0.1
-        new_pos[1] = [arm_len[1] * math.cos(new_ang[1]), arm_len[1] * math.sin(new_ang[1])]
-        hinge1_target_len = calc_len([r, z], new_pos[1])
+        arm1_ang -= (arm1_ang - target_ang) * 0.1
+        arm1_pos = [arm_len[1] * math.cos(arm1_ang), arm_len[1] * math.sin(arm1_ang)]
+        hinge1_target_len = calc_len([r, z], arm1_pos)
 
-    # Update second hinge's angle and position
-    new_ang[2] = calc_ang(target_len, arm_len[1], hinge1_target_len) + calc_ang(arm_len[3], arm_len[2], hinge1_target_len)
-    new_pos[2] = [new_pos[1][0] + arm_len[2] * math.cos(new_ang[1] + new_ang[2] - math.pi),
-                  new_pos[1][1] + arm_len[2] * math.sin(new_ang[1] + new_ang[2] - math.pi)]
+    # Calculate second hinge's angle
+    arm2_ang = calc_ang(target_len, arm_len[1], hinge1_target_len) + calc_ang(arm_len[3], arm_len[2], hinge1_target_len)
 
-    # Update third hinge's angle and position
-    new_ang[3] = calc_ang(hinge1_target_len, arm_len[2], arm_len[3])
-    new_pos[3] = [new_pos[2][0] + arm_len[3] * math.cos(new_ang[1] + new_ang[2] + new_ang[3] - (2 * math.pi)),
-                  new_pos[2][1] + arm_len[3] * math.sin(new_ang[1] + new_ang[2] + new_ang[3] - (2 * math.pi))]
+    # Calculate third hinge's angle
+    arm3_ang = calc_ang(hinge1_target_len, arm_len[2], arm_len[3])
 
-    # Update fourth hinge's angle and positon
-    new_ang[4] = (3 * math.pi) - new_ang[1] - new_ang[2] - new_ang[3]
-    new_pos[4] = new_pos[3]
+    # Calculate fourth hinge's angle
+    arm4_ang = (3 * math.pi) - arm1_ang - arm2_ang - arm3_ang
 
-    # Update hand rotater's angle and position
-    new_ang[5] = math.pi - new_ang[0]
-    new_pos[5] = new_pos[4]
+    # Calculate hand rotater's angle
+    arm5_ang = math.pi - arm0_ang
 
-    return new_ang, new_pos
+    return [arm0_ang, arm1_ang, arm2_ang, arm3_ang, arm4_ang, arm5_ang]
+
+def calc_arm_pos(ang_lis):
+
+    # Set base rotater's position
+    arm0_pos = [0.0, 0.0]
+
+    # Calculate first hinge's position
+    arm1_pos = [arm_len[1] * math.cos(ang_lis[1]),
+                arm_len[1] * math.sin(ang_lis[1])]
+
+    # Calculate second hinge's position
+    arm2_pos = [arm1_pos[0] + arm_len[2] * math.cos(ang_lis[1] + ang_lis[2] - math.pi),
+                arm1_pos[1] + arm_len[2] * math.sin(ang_lis[1] + ang_lis[2] - math.pi)]
+
+    # Calculate third hinge's position
+    arm3_pos = [arm2_pos[0] + arm_len[3] * math.cos(ang_lis[1] + ang_lis[2] + ang_lis[3] - (2 * math.pi)),
+                arm2_pos[1] + arm_len[3] * math.sin(ang_lis[1] + ang_lis[2] + ang_lis[3] - (2 * math.pi))]
+
+    # Set fourth hinge's position
+    arm4_pos = [arm3_pos[0], arm3_pos[1]]
+
+    # Set hand rotater's position
+    arm5_pos = [arm4_pos[0], arm4_pos[1]]
+
+    return [arm0_pos, arm1_pos, arm2_pos, arm3_pos, arm4_pos, arm5_pos]
+
+# Returns a list of angles and positions
+def arm_move_lis(target, step):
+    pass

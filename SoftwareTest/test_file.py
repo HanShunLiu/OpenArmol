debdc7ed7f8ae18b40753aeeb91c8a9255315cd7
Winnie Liu <hl2393@cornell.edu>
2024-08-18 15:25:53 -0400

    Updated to match movement_test.py

--- SoftwareTest/test_file.py
+++ SoftwareTest/test_file.py
@@ -1,14 +1,16 @@
 import movement_test as mt
+import math
 
 # Testing Code
 lis = [[-0.5, 0, 0], [-0.5, 0, 1], [-0.5, 1, 1], [-0.5, 1, 0],
        [0.5, 0, 0], [0.5, 0, 1], [0.5, 1, 1], [0.5, 1, 0], [0, 0, 0]]
 for x in lis:
-    x1, x2 = mt.calc_arm_pos(x[0], x[1], x[2])
+    ang = mt.calc_arm_ang(x[0], x[1], x[2])
+    pos = mt.calc_arm_pos(ang)
 
     new_cord = []
-    for y in x2:
-        z = mt.cyl_to_car(y[0], x1[0], y[1])
+    for y in pos:
+        z = mt.cyl_to_car(y[0], ang[0], y[1])
         new_cord.append([round(z[0],3), round(z[1],3), round(z[2],3)])
 
     stri = ""
@@ -25,5 +27,7 @@ for x in lis:
 
 
     for i in range(1, 4):
-        dis = mt.calc_len_3d(new_cord[i-1], new_cord[i])
+        dis = math.sqrt(((new_cord[i][0] - new_cord[i-1][0])**2) +
+                        ((new_cord[i][1] - new_cord[i-1][1])**2) +
+                        ((new_cord[i][2] - new_cord[i-1][2])**2))
         print(round(dis, 3))
